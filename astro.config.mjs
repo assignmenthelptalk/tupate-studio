4c69dceb20e1f85f3f2e806327f8052c58aed88c
Job Mugachia <assignmenthelptalk@gmail.com>
2026-03-20 21:31:10 +0300

    Fix Astro sitemap build config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,7 @@
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 
-const integrations = process.platform === 'win32' ? [] : [sitemap()];
-
 export default defineConfig({
   site: 'https://tupate.studio',
-  integrations,
+  integrations: [sitemap()],
 });
